0fe42c56ad7429e06704422d1aeae829344513b3
Ubuntu <ubuntu@aoj19037-vm.js2local>
2023-04-26 18:46:41 +0000

    Added jobs route

--- src/app.py
+++ src/app.py
@@ -318,6 +318,19 @@ flight \n       [/map]                                  GET or DELETE plot with 
 b \n       [/help]                                 Return string with information on routes"
      return message
 
-    
+
+@app.route('/jobs', methods=['POST'])
+  def jobs_api():
+      """
+      API route for creating a new job to do some analysis. This route accepts a JSON payload
+      describing the job to be created.
+      """
+      try:
+          job = request.get_json(force=True)
+      except Exception as e:
+          return True, json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
+      return json.dumps(jobs.add_job(job['lower'], job['upper']))
+
+
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
